ce952fe39e958688d228c10ac4e82620720a31ba
AVU4 <VansovichAI@gmail.com>
2023-05-15 17:13:43 +0300

    Added database connection

--- src/load_dataset.py
+++ src/load_dataset.py
@@ -1,9 +1,21 @@
 import pandas as pd
 import sys
+import psycopg2
+from psycopg2 import sql
 
 
 def handle_dataset(filename):
     data = pd.read_csv(filename, delimiter='\t')
+    connection = psycopg2.connect(
+        host="127.0.0.1:5432",
+        database="postgres",
+        user="postgres",
+        password="test"
+    )
+    cursor = connection.cursor()
+    cursor.execute(sql.SQL("CREATE TABLE TEST;"))
+    cursor.close()
+    connection.close()
     print(data)
 
 
