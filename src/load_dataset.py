b89fba3f3938d3781435e0833ad017d3f85ff3df
AVU4 <VansovichAI@gmail.com>
2023-05-15 17:58:49 +0300

    Fixed

--- src/load_dataset.py
+++ src/load_dataset.py
@@ -6,7 +6,7 @@ from database_service import get_connection, create_table
 def handle_dataset(filename):
     data = pd.read_csv(filename, delimiter='\t')
     connection = get_connection()
-    create_table('DATASET_RAW', connection)
+    create_table(connection, 'DATASET_RAW')
     connection.close()
     print(data)
 
