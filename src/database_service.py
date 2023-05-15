5e761cacae274f6c68cf14e0e2da777b65ff7601
AVU4 <VansovichAI@gmail.com>
2023-05-15 21:57:16 +0300

    Made preprocessing data

--- src/database_service.py
+++ src/database_service.py
@@ -1,6 +1,7 @@
 import psycopg2
 import os
 from psycopg2 import sql
+from pandas import DataFrame
 
 
 def get_connection():
@@ -18,7 +19,6 @@ def drop_table(cursor, table_name):
 
 def create_table(connection, table_name):
     cursor = connection.cursor()
-    drop_table(cursor, table_name)
     cursor.execute(sql.SQL(
         "CREATE TABLE {table} ("
         "id int primary key, "
@@ -38,6 +38,30 @@ def save_data(connection, data, table_name):
         cursor.execute(sql.SQL("INSERT INTO {table}(id, title, score, link, summary, published, tickers) values (%s, %s, %s, %s, %s, %s, %s)")
                        .format(table=sql.Identifier(table_name)),
                        (index, row['title'], row['score'], row['link'], row['summary'], row['published'], row['tickers'].replace('[', '{').replace(']', '}')))
+    cursor.close()
+    connection.commit()
+
+
+def get_data_from_table(connection, table_name):
+    cursor = connection.cursor()
+    cursor.execute(sql.SQL("select id, score, summary from {table}").format(table=sql.Identifier(table_name)))
+    result = DataFrame(cursor.fetchall(), index=None)
+    result.set_index(0, inplace=True)
+    cursor.close()
+    return result
+
+
+def create_modified_table(connection, table_name):
+    cursor = connection.cursor()
+    cursor.execute(sql.SQL("CREATE TABLE {table} (id integer primary key, score double precision, summary text)").format(table=sql.Identifier(table_name)))
+    cursor.close()
+    connection.commit()
+
+
+def save_modified_data(connection, data, table_name):
+    cursor = connection.cursor()
+    for index, row in data.iterrows():
+        cursor.execute(sql.SQL("INSERT INTO {table} (id, score, summary) values(%s, %s, %s)").format(table=sql.Identifier(table_name)), (index, row[1], row[2]))
     cursor.close()
     connection.commit()
 
