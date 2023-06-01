0ecfe73da1e3e5ac3655ff92fd48e1f6047d8aad
AVU4 <VansovichAI@gmail.com>
2023-06-01 14:55:58 +0300

    Fix

--- resources/deploy_script.sh
+++ resources/deploy_script.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-pkill app.py
+
+PID_FILE=/tmp/flask_app_pid
+if [ -f "$PID_FILE" ]; then
+    echo "$PID_FILE exists."
+    kill $(cat PID_FILE)
+fi
+
 python3 app.py &
+pid=$?
+echo "$pid" > $PID_FILE
+
+disown
